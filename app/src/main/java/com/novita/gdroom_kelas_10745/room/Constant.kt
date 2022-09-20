7261d4e8838dcd92578eaf0055779dc9c86b3356
novitaaSarii <novitaasarii187@gmail.com>
2022-09-20 15:05:19 +0700

    gd5_room_0745

---
+++ app/src/main/java/com/novita/gdroom_kelas_10745/room/Constant.kt
@@ -0,0 +1,9 @@
+package com.novita.gdroom_kelas_10745.room
+
+class Constant {
+    companion object{
+        const val TYPE_READ = 0
+        const val TYPE_CREATE = 1
+        const val TYPE_UPDATE = 2
+    }
+}
